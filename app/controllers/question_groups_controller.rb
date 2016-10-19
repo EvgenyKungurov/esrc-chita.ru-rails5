96c5118d39e4517bcf5316ae3306fcecc6d827e2
Evgeny Kungurov <evgenii.kungurov@gmail.com>
2016-10-19 13:05:54 +0900

    Migrated to Rails5

--- app/controllers/question_groups_controller.rb
+++ app/controllers/question_groups_controller.rb
@@ -1,19 +1,15 @@
 class QuestionGroupsController < ApplicationController
-  respond_to :html, :json
   before_action :set_question_group, only: [:show, :edit, :update, :destroy]
 
   def index
     @question_groups = QuestionGroup.all
-    respond_with(@question_groups)
   end
 
   def show
-    respond_with(@question_group)
   end
 
   def new
     @question_group = QuestionGroup.new
-    respond_with(@question_group)
   end
 
   def edit
@@ -22,17 +18,14 @@ class QuestionGroupsController < ApplicationController
   def create
     @question_group = QuestionGroup.new(question_group_params)
     @question_group.save
-    respond_with(@question_group)
   end
 
   def update
     @question_group.update(question_group_params)
-    respond_with(@question_group)
   end
 
   def destroy
     @question_group.destroy
-    respond_with(@question_group)
   end
 
   private
