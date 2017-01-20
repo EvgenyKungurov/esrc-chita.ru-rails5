749dbfdf7e756484bd9b1620d0521fbad2753329
EvgenyKungurov <evgenii.kungurov@gmail.com>
2017-01-20 03:23:09 +0000

    Add helper to join conference

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,14 @@
 module SessionsHelper
   def can_do_it?
-    redirect_to root_path unless authenticate_user! && (current_user.has_role? :admin || :editor)
+    unless authenticate_user! && (current_user.has_role? :admin || :editor)
+      redirect_to root_path
+    end
+  end
+
+  def enter_to_conference
+    result = ConferenceService.new(current_user).call
+    if result.success?
+      content_tag(:li, link_to('Подключится к конференции', result.url, target: '_blank'))
+    end
   end
 end
